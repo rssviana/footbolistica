db0075c0e6de492a1dec3ceb7ff01e6c8b1f8a5c
R.Viana <ronanssviana@gmail.com>
2018-05-21 00:26:34 -0300

    first interaction with soccer api
    get competition list and showing on console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "Goddess Symphony";
+  competitions: any;
+  apiURL = "http://api.football-data.org/v1/";
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.competitions = this.http
+      .get(this.apiURL + "competitions/", {
+        headers: new HttpHeaders({
+          "X-Auth-Token": "cd32d0d4baff468894ef60423ca0a292"
+        })
+      })
+      .subscribe(data => {
+        for (var i = 0; i <= 16; i++) {
+          console.log(data[i].id + " - " + data[i].caption);
+        }
+      });
+  }
 }
