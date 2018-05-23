c2d894f42fa09e4dc0e43c95c2dd620449e2da80
rssviana <ronanssviana@gmail.com>
2018-05-23 17:08:44 -0300

    adding api endpoints inside the root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,15 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 })
 export class AppComponent implements OnInit {
   title = "Goddess Symphony";
-  competitions: any;
+  competitions = new Array();
+  teams = new Array();
+  players = new Array();
   apiURL = "http://api.football-data.org/v1/";
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.competitions = this.http
+    this.http // get todas os campeonatos 
       .get(this.apiURL + "competitions/", {
         headers: new HttpHeaders({
           "X-Auth-Token": "cd32d0d4baff468894ef60423ca0a292"
@@ -22,8 +24,53 @@ export class AppComponent implements OnInit {
       })
       .subscribe(data => {
         for (var i = 0; i <= 16; i++) {
-          console.log(data[i].id + " - " + data[i].caption);
+          this.competitions.push(data[i]);
         }
+        console.log(this.competitions);
       });
+
+    this.http // get um time
+      .get(this.apiURL + "teams/66", {
+        headers: new HttpHeaders({
+          "X-Auth-Token": "cd32d0d4baff468894ef60423ca0a292"
+        })
+      })
+      .subscribe( team => {
+        console.log( team )
+      });
+
+    this.http // get todos os times de uma campeonato
+      .get(this.apiURL + "competitions/444/teams", {
+        headers: new HttpHeaders({
+          "X-Auth-Token": "cd32d0d4baff468894ef60423ca0a292"
+        })
+      })
+      .subscribe(allteams => {
+        console.log(allteams);
+      });
+
+      this.http // get todos os jogadores de uma time
+      .get(this.apiURL + "teams/66/players", {
+        headers: new HttpHeaders({
+          "X-Auth-Token": "cd32d0d4baff468894ef60423ca0a292"
+        })
+      })
+      .subscribe(allplayers => {
+        console.log(allplayers);
+      })
+
+      //get tabela de uma liga
+
+      this.http // get todos os jogadores de uma time
+      .get(this.apiURL + "competitions/444/leagueTable", {
+        headers: new HttpHeaders({
+          "X-Auth-Token": "cd32d0d4baff468894ef60423ca0a292"
+        })
+      })
+      .subscribe(leaguetable => {
+        console.log(leaguetable);
+      })
+
+
   }
 }
