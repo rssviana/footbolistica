ef1f7b20c473b175c6811742558059ff2e455e28
R.Viana <ronanssviana@gmail.com>
2018-05-28 13:56:12 -0300

    finish home page style And adding competition page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
+import { CompetitionComponent } from './competition/competition.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CompetitionComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
